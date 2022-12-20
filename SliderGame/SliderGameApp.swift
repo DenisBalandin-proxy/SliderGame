ba64abebc34b7ef0e9eed6d1c11430be325f9412
Denis Balandin <doublestar2013@yandex.ru>
2022-12-20 15:37:51 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Denis on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
